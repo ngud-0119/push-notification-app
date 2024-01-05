3c3757f8b78d181253440644f7ef77243f258d3a
ngud-0119 <finaldev.dream119@gmail.com>
2024-01-05 04:53:08 -0500

    service-worker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 function App() {
   return (
     <div className="App">
